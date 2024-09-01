112c4d30172c63ec0cbc46971b813f60e1d6ff10
James Vickers <jamesvickers19@yahoo.com>
2024-09-01 14:52:01 -0600

    add barcode scanning tab

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -41,6 +41,13 @@ export default function TabLayout() {
           tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
         }}
       />
+      <Tabs.Screen
+        name="barcodeScanner"
+        options={{
+          title: "Barcode",
+          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+        }}
+      />
     </Tabs>
   );
 }
